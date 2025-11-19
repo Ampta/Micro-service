dd1a9621b1ce0ff68617604ff693adcdd60192e6
nakanu <ampta77000@gmail.com>
2025-11-19 17:04:27 +0530

    initial set-up for activity-service

---
+++ activity-service/src/main/java/com/ampta/activity_service/controller/ActivityController.java
@@ -0,0 +1,33 @@
+package com.ampta.activity_service.controller;
+
+import com.ampta.activity_service.dto.ActivityRequest;
+import com.ampta.activity_service.dto.ActivityResponse;
+import com.ampta.activity_service.service.ActivityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/activities")
+@RequiredArgsConstructor
+public class ActivityController {
+
+    private final ActivityService activityService;
+
+    @PostMapping
+    public ResponseEntity<ActivityResponse> trackActivity(@RequestBody ActivityRequest request){
+        return ResponseEntity.ok(activityService.trackActivity(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ActivityResponse>> getUserActivities(@RequestHeader("X-USER-ID") String userId){
+        return ResponseEntity.ok(activityService.getUserActivities(userId));
+    }
+
+    @GetMapping("/{activityId}")
+    public ResponseEntity<ActivityResponse> getActivity(@PathVariable String activityId){
+        return ResponseEntity.ok(activityService.getActivityById(activityId));
+    }
+}
---
+++ activity-service/src/main/java/com/ampta/activity_service/repository/ActivityRepository.java
@@ -0,0 +1,11 @@
+package com.ampta.activity_service.repository;
+
+import com.ampta.activity_service.model.Activity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ActivityRepository extends MongoRepository<Activity, String> {
+
+    List<Activity> findByUserId(String userId);
+}
