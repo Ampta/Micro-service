567747ffd9b8764df4ac2bb0d9be87de75bb60c1
nakanu <ampta77000@gmail.com>
2025-11-20 09:22:10 +0530

    ai-service is added

---
+++ ai-service/src/main/java/com/ampta/ai_service/service/RecommendationService.java
@@ -0,0 +1,25 @@
+package com.ampta.ai_service.service;
+
+import com.ampta.ai_service.model.Recommendation;
+import com.ampta.ai_service.repository.RecommendationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RecommendationService {
+
+    private final RecommendationRepository recommendationRepository;
+
+
+    public List<Recommendation> getUserRecommendation(String userId) {
+        return recommendationRepository.findByUserId(userId);
+    }
+
+    public Recommendation getActivityRecommendation(String activityId) {
+        return recommendationRepository.findByActivityId(activityId)
+                .orElseThrow(() -> new RuntimeException("No Recommendation found for this activity: " + activityId));
+    }
+}
